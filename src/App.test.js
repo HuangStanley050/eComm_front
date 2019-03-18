cd09e31e9223013b0e24f0a1c7ef55e856dfe9de
Stanley Huang <infamous_godhand@yahoo.com>
2019-03-18 09:10:22 +0000

    first test to see if Navbar component shows up in App

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import NavBar from "./components/navigation/navbar";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from "./App";
+
+test("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy();
+});
+
+test("renders a navbar", () => {
+  const wrapper = shallow(<App />);
+  //console.log(wrapper.debug());
+  expect(wrapper.contains(<NavBar />)).toBe(true);
 });
