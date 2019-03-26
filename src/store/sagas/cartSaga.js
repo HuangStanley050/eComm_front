5ec7c9389fd50305308804a96a33cbaf385f2d7b
Stanley Huang <infamous_godhand@yahoo.com>
2019-03-26 09:47:23 +0000

    added token to request when making purchase

--- src/store/sagas/cartSaga.js
+++ src/store/sagas/cartSaga.js
@@ -1,14 +1,21 @@
 import { takeEvery, put } from "redux-saga/effects";
 import * as actionType from "../actions/actionTypes";
-
+/*global localStorage */
 import { make_payment_success } from "../actions/cartAction";
 import API from "../../config/api";
 import axios from "axios";
 
 function* cartSagaPaymentWorker(action) {
-  //yield console.log(action.paymentInfo);
   try {
-    const result = yield axios.post(API.pay, action.paymentInfo);
+    const result = yield axios({
+      method: "post",
+      url: API.pay,
+      data: action.paymentInfo,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.token
+      }
+    });
     //console.log(result);
     yield put(make_payment_success());
   } catch (e) {
